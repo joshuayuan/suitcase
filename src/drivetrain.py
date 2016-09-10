83e36da3be650d9dc421188c3121a7c1b2a9133e
Joshua Yuan <joshua43210@gmail.com>
2016-09-11 08:46:47 +0930

    Bug fixes to make suitcase launchable

--- src/drivetrain.py
+++ src/drivetrain.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
@@ -8,18 +10,18 @@ class Drivetrain(object):
     wheel_center_distance = 106.9 #in mm
     wheel_diameter = 83 #in mm
     def __init__(self):
-        self.pubL = rospy.Publisher("/motor_command_left/rpm",std_msgs.Float64, queue_size=2)
-        self.pubR = rospy.Publisher("/motor_command_right/rpm", std_msgs.Float64, queue_size=2)
-        self.sub = rospy.Subscriber("/follower_velocity_smoother/raw_cmd_vel", geometry_msgs.Twist, twistCallback)
+        self.pubL = rospy.Publisher("/motor_command_left/rpm",Float64, queue_size=2)
+        self.pubR = rospy.Publisher("/motor_command_right/rpm", Float64, queue_size=2)
+        self.sub = rospy.Subscriber("/follower_velocity_smoother/raw_cmd_vel", Twist, self.twistCallback)
 
     def twistCallback(self, data):
         fwd_speed = data.linear.x
         turn_speed = data.angular.z
-        left_motor = fwd_speed + turn_speed * wheel_center_distance
-        right_motor = fwd_speed - turn_speed * wheel_center_distance
+        left_motor = fwd_speed + turn_speed * self.wheel_center_distance
+        right_motor = fwd_speed - turn_speed * self.wheel_center_distance
         
-        left_rpm = left_motor / (pi * wheel_diameter)
-        right_rpm = right_motor / (pi * wheel_diameter)
+        left_rpm = left_motor / (pi * self.wheel_diameter)
+        right_rpm = right_motor / (pi * self.wheel_diameter)
 
         self.pubL.publish(left_rpm)
         self.pubR.publish(right_rpm)
